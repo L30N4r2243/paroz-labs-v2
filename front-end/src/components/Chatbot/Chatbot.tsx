059410074a57d8135b280b5a7775f73bc9c89bb4
L30N4r2243 <leonardomagnaldi@gmail.com>
2025-10-30 10:26:39 -0300

    mod conection variable de entorno

--- front-end/src/components/Chatbot/Chatbot.tsx
+++ front-end/src/components/Chatbot/Chatbot.tsx
@@ -79,7 +79,7 @@ export default function Chatbot() {
     setMessages((prev) => [...prev, { sender: "user", text: message }])
 
     try {
-      const res = await fetch("http://localhost:5000/api/chat", {
+      const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/chat`, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({
